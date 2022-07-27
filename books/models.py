f51c293ed2efd07e69c23d3998d182736ab6b6ac
Pranshu1902 <pranshu1902@gmail.com>
2022-07-28 01:08:45 +0530

    renewed migrations

--- books/models.py
+++ books/models.py
@@ -12,7 +12,6 @@ STATUS_CHOICES = (
 )
 
 class Book(models.Model):
-    # id = models.UUIDField(primary_key=True, unique=True)#, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     image = models.ImageField(default="default.png", upload_to="book_images")
@@ -22,6 +21,7 @@ class Book(models.Model):
     timeTaken = models.IntegerField(default=0)
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default=STATUS_CHOICES[0][0])
 
+    # id = models.UUIDField(primary_key=True, unique=True)#, default=uuid.uuid4, editable=False)
     # reading = models.BooleanField(default=False)
     # date_started = models.DateField(auto_now_add=True, null=True, blank=True)
     # date_finished = models.DateField(null=True, blank=True)
