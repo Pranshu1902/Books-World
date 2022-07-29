e7eae4ea7c5b5f2069e62400be135fa4d76ab258
Pranshu1902 <pranshu1902@gmail.com>
2022-07-29 17:15:44 +0530

    added default image

--- books/models.py
+++ books/models.py
@@ -14,7 +14,7 @@ STATUS_CHOICES = (
 class Book(models.Model):
     name = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
-    imageLink = models.CharField(max_length=3500, default="none")
+    imageLink = models.CharField(max_length=3500, default="http://books-world-pranshu1902.herokuapp.com/static/default.png")
     user = models.ForeignKey(User, related_name="books", on_delete=models.CASCADE, null=True, blank=True)
     totalPages = models.IntegerField(default=0)
     pagesRead = models.IntegerField(default=0)
